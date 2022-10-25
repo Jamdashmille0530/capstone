09cf61df8cf4de4bd813ef09041d514af02a4a3e
Vincent Lennuel Llanto <72075830+soulevil098123@users.noreply.github.com>
2022-10-26 02:10:17 +0800

    Remove validate in function

--- backend/src/controllers/image.controller.js
+++ backend/src/controllers/image.controller.js
@@ -1,24 +1,7 @@
 import { getImageLocation } from '../services/upload.services'
 import { findUserById, updateUserData } from '../services/user.services'
 
-const requiredColumns = [
-  'idPicture',
-  'gradeSlip',
-  'eslip',
-  'ncae',
-  'ebill',
-  'wbill',
-  'tbill',
-  'bcert',
-  'indigency',
-  'autobiography',
-  'pantawidId',
-  'sketchAdress',
-]
-
 export const insertImage = async (req, res) => {
-  if (!requiredColumns.includes(req.params.column))
-    return res.status(404).json({ message: 'Not Found' })
   const { userId } = req.payload
   const data = getImageLocation(req)
   const updated = await updateUserData(userId, { [req.params.column]: data })
@@ -26,8 +9,6 @@ export const insertImage = async (req, res) => {
 }
 
 export const getImage = async (req, res) => {
-  if (!requiredColumns.includes(req.params.column))
-    return res.status(404).json({ message: 'Not Found' })
   const data = await findUserById(req.params.id)
   data[req.params.column]
   res.sendFile(data[req.params.column], {
