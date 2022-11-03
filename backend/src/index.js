51fad663ada0c4ab0a7b9bcab653c4fd1f383bc3
Glendell <dellbringino@gmail.com>
2022-11-03 20:27:19 +0800

    add socket.io

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,38 +7,48 @@ import authRoute from './routes/auth.routes'
 import calendarRoute from './routes/calendar.routes'
 import adminRoute from './routes/admin.routes'
 
+const http = require('http')
+const { Server } = require('socket.io')
+
 const app = express()
 
-const server = async () => {
-  app.use(express.static('public/images'))
-  app.use(express.json())
-  app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
+const server = http.createServer(app)
 
-  // Routes
-  app.use('/api/user', userRoute)
-  app.use('/api/auth', authRoute)
-  app.use('/api/calendar', calendarRoute)
-  app.use('/api/admin', adminRoute)
+app.use(express.static('public/images'))
+app.use(express.json())
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
 
-  // Testing
-  app.get('/api/healthchecker', (_, res) => {
-    res.status(200).json({
-      status: 'success',
-      message: 'Welcome to NodeJs with Prisma and PostgreSQL',
-    })
+// Routes
+app.use('/api/user', userRoute)
+app.use('/api/auth', authRoute)
+app.use('/api/calendar', calendarRoute)
+app.use('/api/admin', adminRoute)
+
+// Testing
+app.get('/api/healthchecker', (_, res) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'Welcome to NodeJs with Prisma and PostgreSQL',
   })
+})
 
-  const PORT = process.env.PORT || 5000
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+})
 
-  app.listen(PORT, () =>
-    console.log(`🚀 Server ready at: http://localhost:${PORT}`)
-  )
-}
+io.on('connection', (socket) => {
+  console.log(`User connected: ${socket.id}`)
 
-server()
-  .catch((err) => {
-    throw err
+  // this will listen to the event send_message declare in the FE
+  socket.on('send_message', (data) => {
+    socket.broadcast.emit('receive_message', data)
   })
-  .finally(async () => {
-    await prisma.$disconnect()
-  })
+})
+
+const PORT = process.env.PORT || 5000
+
+server.listen(PORT, () =>
+  console.log(`🚀 Server ready at: http://localhost:${PORT}`)
+)
