4601321a1ccd26793b684a25a94ff973f9fbf357
Jamdashmille0530 <qjpacabrera@tip.edu.ph>
2022-12-04 23:56:48 +0800

    wss

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,4 +1,7 @@
 import io from 'socket.io-client'
-export const socket = io('ws://capstone-2bvms2n4d-jamdashmille0530.vercel.app', {
-  transports: ['websocket', 'polling'],
-})
+export const socket = io(
+  'wss://capstone-2bvms2n4d-jamdashmille0530.vercel.app',
+  {
+    transports: ['websocket', 'polling'],
+  }
+)
