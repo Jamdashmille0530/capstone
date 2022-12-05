068d0bf6b891d6197b1412ab3bfd8c53db3150ef
Vincent Lennuel Llanto <72075830+soulevil098123@users.noreply.github.com>
2022-12-06 00:37:01 +0800

    Edit sockets

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,4 +1,4 @@
 import io from 'socket.io-client'
-export const socket = io(process.env.REACT_APP_SOCKET, {
+export const socket = io(process.env.REACT_APP_SOCKET_URL, {
   transports: ['websocket', 'polling'],
 })
