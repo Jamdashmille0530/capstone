428cb7b099a19f9df18e8d6375862f22fc4611b7
Jamdashmille0530 <qjpacabrera@tip.edu.ph>
2022-12-12 15:00:41 +0800

    try lamang

--- frontend/src/pages/Home/admin/Application/index.jsx
+++ frontend/src/pages/Home/admin/Application/index.jsx
@@ -300,8 +300,7 @@ const Application = () => {
                     <td class="text-sm text-gray-900 font-light px-2 py-2 whitespace-nowrap">
                       <button
                         className="px-3 py-2 mt-4 border-green-700 text-black bg-green-300 rounded-lg hover:bg-gray-400 "
-                        onClick={async () => {
-                                await acceptApplicant(user.id, user.email)
+                        onClick={async () => 
 
                           swal({
                             title: 'Are you sure?',
@@ -309,16 +308,21 @@ const Application = () => {
                             icon: 'warning',
                             buttons: true,
                             dangerMode: true,
-                          }).then((acceptApplicant) => {
-                            if (acceptApplicant) {
+                          }).then((willDelete) => {
+                            
+                            if (willDelete) {
+                        
+                              {
+                                await acceptApplicant(user.id, user.email)
+                              
                               swal('Accepted, sent to email', {
                                 icon: 'success',
-                              })
+                              })}
                             } else {
                               swal('We can try again next time')
                             }
                           })
-                        }}
+                        }
                       >
                         Approved
                       </button>
